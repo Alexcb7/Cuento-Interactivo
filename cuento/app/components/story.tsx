28e43799f1e653a277c50cbe47ef65de7da79acd
Alexcb7 <alex.cortell.ifp@gmail.com>
2026-02-17 11:54:21 +0100

    commit sections completo

--- cuento/app/components/story.tsx
+++ cuento/app/components/story.tsx
@@ -5,6 +5,12 @@ import Section02 from "@/app/components/sections/section02"
 import Section03 from "@/app/components/sections/section03"
 import Section04 from "@/app/components/sections/section04"
 import Section05 from "@/app/components/sections/section05"
+import Section06 from "@/app/components/sections/section06"
+import Section07 from "@/app/components/sections/section07"
+import Section08 from "@/app/components/sections/section08"
+import Section09 from "@/app/components/sections/section09"
+import Section10 from "@/app/components/sections/section_final"
+
 
 export default function Story() {
   return (
@@ -14,6 +20,12 @@ export default function Story() {
       <Section03 />
       <Section04 />
       <Section05 />
+      <Section06 />
+      <Section07 />
+      <Section08 />
+      <Section09 />
+      <Section10 />
+      
     </main>
   )
 }
